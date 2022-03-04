2b21976c75007ca23a90b6204d69298463896f6d
LizzieSevC <lizette.sevillano.c@gmail.com>
2022-03-04 12:59:13 -0500

    footer update

--- app/src/components/header_footer/Footer.jsx
+++ app/src/components/header_footer/Footer.jsx
@@ -2,6 +2,7 @@ import Corporation from '../../assets/img/corporation.svg';
 import LogoMiguel from '../../assets/img/miguel logo.png';
 import LogoLiz from '../../assets/img/Lizzie logo.png';
 import LogoSanti from '../../assets/img/LogoSantiago.svg';
+import LogoDani from '../../assets/img/LogoDani.png';
 import './Footer.css';
 
 
@@ -20,7 +21,8 @@ const Footer = () => {
     <div className="devFooter">
     <a href='https://github.com/Miguel-Lopez-97'><img className="developerLogo" src={LogoMiguel} alt={'LogoMiguel'}/></a>
     <a href='https://github.com/LizzieSevC'><img className="developerLogo" src={LogoLiz} alt={'LogoLizzie'}/></a>
-    <a href='https://github.com/santilopezco'><img className='developerLogo' src={LogoSanti} alt={'logosanti'} /></a>
+    <a href='https://github.com/santilopezco'><img className='developerLogo' src={LogoSanti} alt={'logosanti'}/></a>
+    <a href='https://github.com/DanielKatt'><img className='developerLogo' src={LogoDani} alt={'logoDani'}/></a>
     </div>
 
 </>
