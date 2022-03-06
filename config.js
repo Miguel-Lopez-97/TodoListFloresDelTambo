0dbc8b4c50261b0024bf7b50ab094f8d5244d38c
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-03-06 16:27:48 -0500

    Update modules deploy

---
+++ config.js
@@ -0,0 +1,9 @@
+// import dotenv for local variables
+const dotenv = require('dotenv').config();
+
+// export this module to use it on index
+module.exports = {
+    NODE_ENV: process.env.NODE_ENV || 'local_development',
+    URI: 'mongodb+srv://root:12345@cluster0.x3ien.mongodb.net/todo?retryWrites=true&w=majority',
+    PORT: process.env.PORT || 27017
+}
--- index.js
+++ index.js
@@ -1,26 +1,40 @@
+// Import express, mongoose, config and cors 
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const mongoose = require("mongoose"); // for creating mongoose model (schema)
+const cors = require("cors"); // enable cross origin request
+const config = require("./config")
+const path = require("path")
 
-const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 27017;
+// Initialize app
 const app = express();
 
+// Import router
 const todoRoutes = require("./routes/todoRoutes");
 
-require('dotenv').config({path: 'variables.env'})
-//cors acts as a mediator
+// Mongoose 6 always behaves as if  useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false 
+const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
+
+// Initialize express and  cross origin request
 app.use(express.json());
 app.use(cors());
 
-//connect to the database
-mongoose.connect(process.env.DB_URL,{useNewUrlParser:true})
+//Connet to database (second parameter is opional but is used in order to avoid  warnings in the terminal for previous verions)
+mongoose.connect(config.URI, connectionOptions)
+    // if connection is succesful we want to call app and show a msg in console that it is working
     .then(() => console.log("Connected successfully"))
+    // if connection fails, show error msg
     .catch((err) => console.error(err));
 
 app.use("/todos", todoRoutes);
 
-//verifies if the server is running
-app.listen(PORT,  () => {
-    console.log("The server is listening on port " + PORT);
+console.log(`NODE_ENV=${config.NODE_ENV}`)
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(config.PORT, () => {
+    console.log(`The server is listening on port: ${config.PORT}`);
 });
