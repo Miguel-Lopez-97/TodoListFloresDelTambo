058537b6850e6eaef3ef652585cca10a76d36573
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-03-06 18:21:30 -0500

    commenting url in axios for local changes

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,10 +1,10 @@
-/* import axios from "axios";
+import axios from "axios";
 
 //connects axios with the back(local-host)
-export default axios.create({baseURL:"http://localhost:27017"}); */
+export default axios.create({baseURL:"http://localhost:27017"});
 
-import axios from "axios";
+/* import axios from "axios";
 // create an instance of axios to get data from api without writing full URL
 export default axios.create({
   baseURL: "https://todolistfloresdeltambo.herokuapp.com",
-});
+}); */
